1b5c56b607cd9e4048df1d401d853f38a5b7f5e3
AmbuTransport-SRL <ambutransportsrl@gmail.com>
2026-09-06 16:00:27 +0200

    Correction du responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
+import { createPortal } from 'react-dom';
+
 export default function Footer() {
-  return (
-    <footer className="fixed bottom-0 left-0 w-full bg-black py-4 border-t border-gray-800 text-center z-40">
+  return createPortal(
+    <footer className="fixed bottom-0 left-0 w-full bg-black py-4 border-t border-gray-800 text-center z-50">
       <p className="text-gray-300 font-bold text-sm">
         © {new Date().getFullYear()} Théo Stradiot. Tous droits réservés.
       </p>
-    </footer>
+    </footer>,
+    document.body
   );
 }
