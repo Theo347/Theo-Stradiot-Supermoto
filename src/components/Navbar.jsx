d8b4871ee95bad1223c033426d4a28f97c0502d6
AmbuTransport-SRL <ambutransportsrl@gmail.com>
2026-09-06 16:42:59 +0200

    Correction contraste couleur mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex-shrink-0 font-bold text-2xl tracking-wider font-extrabold leading-tight text-white">
-            THÉO<span className="text-red-brand"> STRADIOT</span>
+            THÉO<span className="to-red-500"> STRADIOT</span>
           </Link>
           
           {/* Menu PC */}
